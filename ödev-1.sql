6bc19e41d7d56e86b1647f11094621c14a7148f2
Melih <81979457+Melih-cell@users.noreply.github.com>
2022-06-28 12:06:18 +0300

    Update ödev-1.sql

--- ödev-1.sql
+++ ödev-1.sql
@@ -10,11 +10,11 @@ AND length < 75;
 SELECT * FROM film
 WHERE rental_rate = 0.99
 AND (replacement_cost = 12.99
-OR replacement_cost = 28.99)
+OR replacement_cost = 28.99);
 
 --customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 SELECT last_name FROM customer
-WHERE first_name = 'Mary'
+WHERE first_name = 'Mary';
 
 --film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 SELECT * FROM film
