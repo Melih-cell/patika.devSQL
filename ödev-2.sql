be1189e0afba90e6d8c2cc4a49772f5d900c391b
Melih <81979457+Melih-cell@users.noreply.github.com>
2022-06-28 12:54:14 +0300

    Update ödev-2.sql

--- ödev-2.sql
+++ ödev-2.sql
@@ -1,7 +1,7 @@
 -- -- film tablosunda bulunan tüm sütunlardaki verileri
 -- replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
 SELECT * FROM film
-WHERE replacement_cost BETWEEN 12.99 AND 16.99
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 
 --actor tablosunda bulunan first_name ve last_name sütunlardaki verileri
 --first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
